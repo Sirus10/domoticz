edbc7b3e8af31bbeed2339b07cb679e90b1e7f34
Sirus10 <coindre_e@hotmail.fr>
2015-08-23 11:11:03 +0200

    Update updateConsoEau_M-1.sh

--- updateConsoEau_M-1.sh
+++ updateConsoEau_M-1.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/ksh
 #######################################################################################
-# This script will get the data for previous mounth 
-# from SDEI/SOGEST website and insert into domoticz.db
+# This script will get the data for previous month 
+# from SDEI/SOGEST (and others) website and insert into domoticz.db
 # It requiere updateConsoEau.sh to be setup.
 # Site    : http://domotique.web2diz.net/
 # Detail  : http://domotique.web2diz.net/?p=131
+# List of working provider : http://domotique.web2diz.net/?p=320
 # Source  : https://github.com/Sirus10/domoticz/blob/master/updateConsoEau_M-1.sh
 # License : CC BY-SA 4.0
 #
