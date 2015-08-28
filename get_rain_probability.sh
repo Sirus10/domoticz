ec3dd166c31ce75f9ebba2e44af92e485401df80
Sirus10 <coindre_e@hotmail.fr>
2015-08-28 19:04:32 +0200

    Create get_rain_probability.sh

---
+++ get_rain_probability.sh
@@ -0,0 +1,62 @@
+
+#!/usr/bin/ksh
+#######################################################################################
+# This script will get weather date from wunderground API and then add the
+# propability of rain into domoticz % devices.
+# Site    : http://domotique.web2diz.net/
+# Source  : https://github.com/Sirus10/domoticz/blob/master/get_rain_probability.sh
+# License : CC BY-SA 4.0
+#
+#  Usage:
+#  ./get_rain_probability.sh
+#
+#######################################################################################
+# Setup START
+COUNTRY=FRANCE
+APIKEY=  # put your api key here
+TOWN=    # put your town here 
+IDX_1h=144    # Update according to your domoticz % divices
+IDX_5h=153    # Update according to your domoticz % divices
+IDX_12h=189   # Update according to your domoticz % divices
+IDX_24h=190   # Update according to your domoticz % divices
+
+DOMO_HTTP=$DOMO_HTTP
+# Setup END
+
+curl -f "http://api.wunderground.com/api/6fae56bbbe7f85f3/hourly/lang:FR/q/France/$TOWN.xml" -o /tmp/weather$TOWN.out
+
+x=1 # Hour
+proba_rain=`grep "pop" /tmp/weather$TOWN.out | sed "s/<pop>// "|sed "s/<\/pop>//" |awk 'NR==1' `
+echo Proba Rain in $x h :  $proba_rain %
+url="http://$DOMO_HTTP/json.htm?type=command&param=udevice&idx=$IDX_1h&nvalue=0&svalue="$proba_rain
+
+url=`echo $url |sed "s/ //"`
+echo $url
+curl -f $url
+
+x=5 # Hour
+proba_rain=`grep "pop" /tmp/weather$TOWN.out | sed "s/<pop>// "|sed "s/<\/pop>//" |awk 'NR==5' `
+echo Proba Rain in $x h :  $proba_rain %
+url="http://$DOMO_HTTP/json.htm?type=command&param=udevice&idx=$IDX_5h&nvalue=0&svalue="$proba_rain
+
+url=`echo $url |sed "s/ //"`
+echo $url
+curl -f $url
+
+x=12 # Hour
+proba_rain=`grep "pop" /tmp/weather$TOWN.out.out | sed "s/<pop>// "|sed "s/<\/pop>//" |awk 'NR==12' `
+echo Probal Rain in $x h :  $proba_rain %
+url="http://$DOMO_HTTP/json.htm?type=command&param=udevice&idx=$IDX_12h&nvalue=0&svalue="$proba_rain
+
+url=`echo $url |sed "s/ //"`
+echo $url
+curl -f $url
+
+x=24 # Hour
+proba_rain=`grep "pop" /tmp/weather$TOWN.out | sed "s/<pop>// "|sed "s/<\/pop>//" |awk 'NR==24' `
+echo Probal pluie dans $x h :  $proba_rain %
+url="http://$DOMO_HTTP/json.htm?type=command&param=udevice&idx=$IDX_24h&nvalue=0&svalue="$proba_rain
+
+url=`echo $url |sed "s/ //"`
+echo $url
+curl -f $url
