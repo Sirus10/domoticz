b5c3ee01b7b10334238c8a6ab720409c90b962ff
Sirus10 <coindre_e@hotmail.fr>
2015-08-28 19:08:59 +0200

    Update get_rain_probability.sh

--- get_rain_probability.sh
+++ get_rain_probability.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/ksh
 #######################################################################################
 # This script will get weather date from wunderground API and then add the
@@ -14,16 +13,16 @@
 # Setup START
 COUNTRY=FRANCE
 APIKEY=  # put your api key here
-TOWN=    # put your town here 
+TOWN=PARIS    # put your town here 
 IDX_1h=144    # Update according to your domoticz % divices
 IDX_5h=153    # Update according to your domoticz % divices
 IDX_12h=189   # Update according to your domoticz % divices
 IDX_24h=190   # Update according to your domoticz % divices
 
-DOMO_HTTP=$DOMO_HTTP
+DOMO_HTTP=192.168.1.16:8080
 # Setup END
 
-curl -f "http://api.wunderground.com/api/6fae56bbbe7f85f3/hourly/lang:FR/q/France/$TOWN.xml" -o /tmp/weather$TOWN.out
+curl -f "http://api.wunderground.com/api/$APIKEY/hourly/lang:FR/q/France/$TOWN.xml" -o /tmp/weather$TOWN.out
 
 x=1 # Hour
 proba_rain=`grep "pop" /tmp/weather$TOWN.out | sed "s/<pop>// "|sed "s/<\/pop>//" |awk 'NR==1' `
@@ -44,7 +43,7 @@ echo $url
 curl -f $url
 
 x=12 # Hour
-proba_rain=`grep "pop" /tmp/weather$TOWN.out.out | sed "s/<pop>// "|sed "s/<\/pop>//" |awk 'NR==12' `
+proba_rain=`grep "pop" /tmp/weather$TOWN.out | sed "s/<pop>// "|sed "s/<\/pop>//" |awk 'NR==12' `
 echo Probal Rain in $x h :  $proba_rain %
 url="http://$DOMO_HTTP/json.htm?type=command&param=udevice&idx=$IDX_12h&nvalue=0&svalue="$proba_rain
 
